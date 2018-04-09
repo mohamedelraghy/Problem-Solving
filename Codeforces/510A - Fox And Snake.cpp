9c4bc07872a41ff61c08c592076dc57dc998b4c5
Mohamed EL-Raghy <elraghy8@gmail.com>
2018-04-09 22:04:25 +0200

    Create 510A - Fox And Snake.cpp

---
+++ Codeforces/510A - Fox And Snake.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n, m;
+	bool l = false, r = true;
+	cin >> n >> m;
+	
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			if(i % 2 == 1) cout << '#';
+			else{
+				if(r && j == m) cout << '#'; 
+				else if(l && j == 1) cout << '#';
+				else cout << '.';
+			}
+		}
+		if(i % 2 == 0){
+			if(l){
+				l = false;
+				r = true;
+			}else if(r){
+				r = false;
+				l = true;
+			}
+		}	
+			
+		cout << "\n";
+	}	
+}
