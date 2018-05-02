0a0cde3718962bce31527d843932f2e87ac4ecbf
Mohamed EL-Raghy <elraghy8@gmail.com>
2018-05-02 23:55:27 +0200

    Create 592B - The Monster and the Squirrel.cpp

---
+++ Codeforces/592B - The Monster and the Squirrel.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	long long n;
+	cin >> n;
+	
+	cout << (n - 3) * (n - 1) + 1 << endl;
+}
