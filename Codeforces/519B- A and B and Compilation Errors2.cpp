bb550a7368522bae3dd9916f386dc60d2028ba9e
mohamedelraghy <elraghy8@gmail.com>
2019-12-02 23:08:22 +0200

    anthor accpted ✔️

---
+++ Codeforces/519B- A and B and Compilation Errors2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ll a, b, c;
+    a = b = c = 0;
+
+    int n, val;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> val;
+        a += val;
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        cin >> val;
+        b += val;
+    }
+    for (int i = 0; i < n-2; i++)
+    {
+        cin >> val;
+        c += val;
+    }
+
+    cout << a - b << "\n" << b - c << endl;
+}
