fd5a7d3f0d5161633cf8a71d655e4727d7694ad0
mohamedelraghy <elraghy8@gmail.com>
2022-04-23 11:11:16 +0200

    accepted

---
+++ Codeforces/1669B -  Triple.rb
@@ -0,0 +1,20 @@
+t = gets.chomp().to_i
+
+while t > 0
+    n = gets.chomp().to_i
+    a = Array.new(n + 1, 0)
+    ind = gets.chomp.split(' ').map!{ |num| num.to_i}
+    
+    ans = -1
+    
+    ind.each do |i|
+        a[i] +=  1
+        if a[i] == 3
+            ans = i
+        end
+    end
+    
+    puts ans
+    t -= 1
+end
+
