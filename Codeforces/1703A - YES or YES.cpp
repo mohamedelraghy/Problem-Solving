5049d4949aceb239fffa9f8707801e6d172e11aa
mohamed El-Raghy <elraghy8@gmail.com>
2022-09-02 10:55:01 +0200

    accepted

---
+++ Codeforces/1703A - YES or YES.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define P(x) cout << #x << " = { " << x << " }\n"
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define INF 1000000000  
+
+// shortcuts for "common" data types in contests
+typedef long long       ll;
+typedef pair<int, int>  ii;
+typedef vector<ii>      vii;
+typedef vector<int>     vi;
+
+// common memset setting 
+// memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+// memset(arr, 0, sizeof arr);    // to clear array of integers 
+
+
+int main(){
+
+    int t; cin >> t;
+    
+    while (t--)
+    {
+        string s, com;
+        
+        cin >> s;
+        for(int i = 0; i < 3; i++) com += tolower(s[i]);
+
+        if(com == "yes") cout << "YES" << endl;
+        else cout << "NO" << endl; 
+    }
+    
+}
