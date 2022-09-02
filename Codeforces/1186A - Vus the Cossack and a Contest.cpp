615c583dbef038deb08888f03a347c9a0431b0a3
mohamed El-Raghy <elraghy8@gmail.com>
2022-09-02 10:29:34 +0200

    accepted

---
+++ Codeforces/1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define P(x) cout << #x << " = { " << x << " }\n"
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define INF 1000000000  
+
+// shortcuts for "common" data types in contests
+typedef long long       ll;
+typedef pair<int, int>  ii;
+typedef vector<ii>      vii;
+typedef vector<int>     vi;
+
+// common memset setting 
+// memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+// memset(arr, 0, sizeof arr);    // to clear array of integers 
+
+
+int main(){
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    cout << ((m >= n && k >= n) ? "Yes\n" : "No\n");
+    
+}
