e4faddb3cbd7819e42071ca7fd0e9d228cb02fe7
mohamed El-Raghy <elraghy8@gmail.com>
2022-09-02 10:11:59 +0200

    accepted

---
+++ Codeforces/1692A -  Marathon.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define P(x) cout << #x << " = { " << x << " }\n"
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define INF 1000000000  
+
+// shortcuts for "common" data types in contests
+typedef long long       ll;
+typedef pair<int, int>  ii;
+typedef vector<ii>      vii;
+typedef vector<int>     vi;
+
+// common memset setting 
+// memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+// memset(arr, 0, sizeof arr);    // to clear array of integers 
+
+
+int main(){
+    int t; cin >> t;
+
+    while (t--)
+    {
+        vi marthon(4);
+        for (int &e : marthon) cin >> e;
+
+        int a = marthon[0];
+        int count = 0;
+        for(int i = 1; i < 4; i++) if(marthon[i] > a) count++;
+
+        cout << count << endl;
+    }
+    
+}
