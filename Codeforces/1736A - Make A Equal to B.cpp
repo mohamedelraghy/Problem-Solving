b468aea724668ccec3b199c29d60ad9513e16155
mohamed El-Raghy <elraghy8@gmail.com>
2022-11-07 11:45:45 +0200

    accepted

---
+++ Codeforces/1736A - Make A Equal to B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define P(x) cout << #x << " = { " << x << " }\n"
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define INF 1000000000
+#define pb(x) push_back(x);
+
+
+// shortcuts for "common" data types in contests
+typedef long long       ll;
+typedef pair<int, int>  ii;
+typedef vector<ii>      vii;
+typedef vector<int>     vi;
+
+// common memset setting 
+// memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+// memset(arr, 0, sizeof arr);    // to clear array of integers 
+
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+        vi a(n), b(n);
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            sum += a[i];
+        }
+        for(int i = 0; i < n; i++){
+            cin >> b[i];
+            sum -= b[i];
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) ans += (a[i] ^ b[i]);
+
+        ans = min (ans, 1 + abs(sum));
+        cout << ans << endl;
+    }
+
+}
